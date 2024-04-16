f971523d6a7efeda44c04eb276a7fb8eb48fefb5
whatislavs <vlady.lu@gmail.com>
2024-04-16 12:45:38 -0400

    added cars

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,6 +1,8 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # Register your models here.
 
