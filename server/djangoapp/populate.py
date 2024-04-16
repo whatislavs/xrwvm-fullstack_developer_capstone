46f3399c88a5775e5268d82702deca038728d6b8
whatislavs <vlady.lu@gmail.com>
2024-04-16 16:09:59 -0400

    testing under and over intendation for visual

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,7 +13,7 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(CarMake.objects.create(name=data['name'],
-            description=data['description']))
+        description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
