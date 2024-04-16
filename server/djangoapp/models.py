3afe2f248e2692f34b657bca27587f8a2daabc00
whatislavs <vlady.lu@gmail.com>
2024-04-16 15:18:49 -0400

    models.py linter errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -15,11 +15,14 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -28,11 +31,11 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
+    year = models.IntegerField(default=2023, validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
 
