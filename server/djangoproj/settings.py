15e6f9978cf76edd28199ccdfeac5dc922d72105
whatislavs <vlady.lu@gmail.com>
2024-04-16 16:29:19 -0400

    python linter should go through now

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://vladislavslu-8000.' \
-    'theiadockernext-0-labs-prod-theiak8s-4-tor01.' \
+    'https://vladislavslu-8000.'
+    'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
     'proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://vladislavslu-8000.' \
-    'theiadockernext-0-labs-prod-theiak8s-4-tor01.' \
+    'https://vladislavslu-8000.'
+    'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
     'proxy.cognitiveclass.ai'
     ]
 
@@ -102,7 +102,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
